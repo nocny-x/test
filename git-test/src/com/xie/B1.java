26f4b8e6ec874bbf9a7e262643192c7a7bb96636
yuanqiangxie <sherick@126.com>
2013-05-09 16:41:31 +0800

    Add b1

---
+++ git-test/src/com/xie/B1.java
@@ -0,0 +1,13 @@
+package com.xie;
+
+public class B1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("B1....");
+	}
+
+}
